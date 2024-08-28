8b5d185e3a419299486ae714e6d6f0b20bb3fbdb
rafa <rafaelcostatricolor@gmail.com>
2024-08-28 16:14:05 -0300

    imagens mimificadas

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require("gulp");
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin')
 
 function styles(){ //função de compressão e transformação do sass para css
     return gulp.src('./src/styles/*.scss')
@@ -7,8 +8,14 @@ function styles(){ //função de compressão e transformação do sass para css
     .pipe(gulp.dest('./dist/css'));
 }
 
+function images(){
+    return gulp.src('./src/images/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/images'));
+}
 
-exports.default = styles;
+
+exports.default = images
 
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', {ignoreInitial: false}, styles)
